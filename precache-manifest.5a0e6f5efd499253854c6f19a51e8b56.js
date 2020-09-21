74bab261c3fc637f230addcd65b3b82de8901926
Darshan004 <badhandarshan@gmail.com>
2020-09-21 12:21:36 +0530

    Updates

---
+++ precache-manifest.5a0e6f5efd499253854c6f19a51e8b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d314e0bfe2cf37ceff4192ed39be3195",
+    "url": "/React-Strawhats/index.html"
+  },
+  {
+    "revision": "dceabf259273234229f9",
+    "url": "/React-Strawhats/static/css/main.a467ec34.chunk.css"
+  },
+  {
+    "revision": "99a3ea9e9448dc2913f0",
+    "url": "/React-Strawhats/static/js/2.fd9b7cd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Strawhats/static/js/2.fd9b7cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dceabf259273234229f9",
+    "url": "/React-Strawhats/static/js/main.9b3fc6a9.chunk.js"
+  },
+  {
+    "revision": "09c9306930e3fa2ec541",
+    "url": "/React-Strawhats/static/js/runtime-main.32fe18a7.js"
+  }
+]);
